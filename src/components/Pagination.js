1548e47f7ffc33096a888cd0fa9ce0985868860f
gumen <mail@gumen.pl>
2020-07-19 10:53:16 +0200

    Improve pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,16 +6,24 @@ export default function Pagination({ pagesCount, activePage}) {
 
   return (
     <ul>
+      {activePage > 1 ? (
+        <li><Link to={'/?page=' + (activePage - 1)}>{'<'}</Link></li>
+      ) : ''}
+
       {pages.map(page => (
         <li key={page}>
           <Link
-            to={'/?page=' + (page)}
+            to={'/?page=' + page}
             style={{ fontWeight: page === activePage ? 'bold' : 'normal' }}
           >
             {page}
           </Link>
         </li>
       ))}
+
+      {activePage < pagesCount ? (
+        <li><Link to={'/?page=' + (activePage + 1)}>{'>'}</Link></li>
+      ) : ''}
     </ul>
   )
 }
