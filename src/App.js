01d77763d27624c645e060d932c7cc7dcbb219f9
gumen <mail@gumen.pl>
2020-07-22 21:46:51 +0200

    Use styles with normalizer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import 'normalize.css'
+import './App.css'
+
 import MerchantsList from './components/MerchantsList.js'
 import MerchantDetails from './components/MerchantDetails'
 import MerchantAdd from './components/MerchantAdd.js'
